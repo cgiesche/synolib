16b87a290e8908ff037ff110f5ea181cfbc9132a
Christoph Giesche <christoph.giesche@gmx.net>
2013-03-18 21:24:48 +0100

    jo

--- src/main/java/de/perdoctus/synolib/responses/AddUrlResponse.java
+++ src/main/java/de/perdoctus/synolib/responses/AddUrlResponse.java
@@ -1,9 +1,22 @@
 package de.perdoctus.synolib.responses;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 /**
  * User: cgiesche
  * Date: 2/24/12
  * Time: 4:03 PM
  */
 public class AddUrlResponse extends DownloadRedirectorResponse {
+	
+	@JsonProperty("task_error_limit")
+	private String taskErrorLimit;
+	
+	public String getTaskErrorLimit() {
+		return taskErrorLimit;
+	}
+	
+	public void setTaskErrorLimit(String taskErrorLimit) {
+		this.taskErrorLimit = taskErrorLimit;
+	}
 }
